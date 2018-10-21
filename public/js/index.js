c5632bb0e904160f2cc33a8e762e40d9e3ddfc81
Jonathan Gaghan <jpgaghan@gmail.com>
2018-10-21 13:02:44 -0400

    pull

--- public/js/index.js
+++ public/js/index.js
@@ -234,6 +234,18 @@ $(() => {
   
 });
 
+$(document).on("click", ".favevent", () => {
+  var currEle = $(e.currentTarget);
+  var eventdates = currEle[0].childNodes[5].innerText;
+  var eventtime = currEle[0].childNodes[5].dataset.sdate;
+  var eventpics = currEle[0].childNodes[5].dataset.edate;
+  var eventtitle = currEle[0].childNodes[5].innerText;
+  var ticketlink = currEle[0].childNodes[5].dataset.sdate;
+  var city = currEle[0].childNodes[5].dataset.edate;
+  var userid = localStorage.getItem('userid');
+  $.post("/event/favorite", {eventdates, eventtime, eventpics, eventtitle, ticketlink, city, userid}) 
+});
+
 $(document).on("click", ".concerts", (e) => {
   event.preventDefault();
   var currEle = $(e.currentTarget);
@@ -250,7 +262,7 @@ $(document).on("click", ".concerts", (e) => {
   var venue = currEle[0].childNodes[2].innerText;
   $("body").empty();
   city = city.substring(0, city.length - 5);
-  userid = localStorage.getItem('userid')
+  userid = localStorage.getItem('userid');
 $.post("/newconcert", {location,date,time,venue,userid});
   eventData = {
     location,
