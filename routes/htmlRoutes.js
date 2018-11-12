b701b3bb807d9f22491450a3fce41941e6b7497a
Jonathan Gaghan <jpgaghan@gmail.com>
2018-11-12 10:18:39 -0500

    pull

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,12 @@
 var db = require("../models");
 
 module.exports = function (app) {
+  
   // Load index page
   app.get("/", function (req, res) {
-    // db.Example.findAll({}).then(function(dbExamples) {
     res.render("index", {
       msg: "Welcome!"
-      // examples: dbExamples
     });
-    // });
   });
 
   app.get("/artist", function (req, res) {
